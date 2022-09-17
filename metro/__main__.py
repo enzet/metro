fb6a1a08e4a57b34f627ab44fde9bd26752f0c36
Sergey Vartanov <me@enzet.ru>
2022-09-17 21:39:45 +0300

    Fix entry point

--- metro/__main__.py
+++ metro/__main__.py
@@ -2,8 +2,7 @@ import json
 import logging
 from pathlib import Path
 
-from metro.core.map import Map
-from metro.core.system import System
+from metro.core.system import System, Map
 from metro.harvest.wikidata import WikidataCityParser, WikidataParser
 
 
@@ -11,9 +10,9 @@ if __name__ == "__main__":
 
     logging.basicConfig(format="%(levelname)s %(message)s", level=logging.INFO)
 
-    map_: Map = Map()
+    map_: Map = Map("prague_metro")
     map_.local_languages = ["cz"]
-    map_.systems = {"metro": System("metro")}
+    map_.systems = {"metro": System({}, "metro")}
 
     cache_directory: Path = Path("cache")
     cache_directory.mkdir(exist_ok=True)
@@ -23,5 +22,11 @@ if __name__ == "__main__":
     parser = WikidataCityParser(wikidata_parser, map_, {190271: "metro"}, [1877386], 190271, [])
     parser.parse()
 
-    for system in map_.get_systems():
-        print(json.dumps(system.to_structure(), indent=4))
+    output_directory: Path = Path("out") / map_.id_
+    output_directory.mkdir(parents=True, exist_ok=True)
+
+    for system in map_.systems.values():
+        with (output_directory / f"{system.id_}.json").open("w+") as output_file:
+            json.dump(system.serialize(), output_file, indent=4, ensure_ascii=False)
+        with (output_directory / f"{system.id_}.json").open() as input_file:
+            System({}, system.id_).deserialize(json.load(input_file))
